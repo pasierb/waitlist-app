0d6251de3e565f354c5105eb64de026a4dffb62d
Michal Pasierbski <mpasierbski@gmail.com>
2024-08-01 10:18:47 +0200

    Remove footer company section

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -69,13 +69,6 @@
             </p>
         </aside>
         <nav>
-            <h6 class="footer-title">Company</h6>
-            <a class="link link-hover">About us</a>
-            <a class="link link-hover">Contact</a>
-            <a class="link link-hover">Jobs</a>
-            <a class="link link-hover">Press kit</a>
-        </nav>
-        <nav>
             <h6 class="footer-title">Legal</h6>
             <a href="{{route('terms')}}" class="link link-hover">Terms of use</a>
             <a href="{{route('privacy')}}" class="link link-hover">Privacy policy</a>
