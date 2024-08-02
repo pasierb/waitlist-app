d2aa3544e89a9174c1a09c8fd5ba24f86dc64184
Michal Pasierbski <mpasierbski@gmail.com>
2024-08-02 13:53:29 +0200

    Add sentry

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,9 @@
+import * as Sentry from "@sentry/browser";
 import axios from 'axios';
-window.axios = axios;
 
+window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+Sentry.init({
+    dsn: import.meta.env.VITE_SENTRY_DSN_PUBLIC,
+});
