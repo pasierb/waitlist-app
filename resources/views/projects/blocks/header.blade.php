1b565d06d2b3f4bcec3267b48f64986636fc0695
Michal Pasierbski <mpasierbski@gmail.com>
2024-08-17 11:38:30 +0200

    Add header classesd

--- resources/views/projects/blocks/header.blade.php
+++ resources/views/projects/blocks/header.blade.php
@@ -1,3 +1,15 @@
+<?php
+
+use Illuminate\Support\Arr;
+
+$classes = [
+    1 => 'text-5xl',
+];
+$cssClass = Arr::get($classes, $data->level, '');
+?>
+
 <div class="prose text-center mb-4 mt-20">
-    <h{{ $data->level }} class="font-semibold">{!! $data->text  !!}</h{{ $data->level }}>
+    <h{{ $data->level }} class="font-semibold {{$cssClass}}">
+    {!! $data->text  !!}
+    </h{{ $data->level }}>
 </div>
