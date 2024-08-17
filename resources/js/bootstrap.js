4171f31df73d9ac9368b9d03703b0c2614df953a
Michal Pasierbski <mpasierbski@gmail.com>
2024-08-17 11:44:54 +0200

    Add Sentry feedback on all envs

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -4,14 +4,12 @@ import axios from 'axios';
 window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-if (import.meta.env.APP_ENV === 'production') {
-    Sentry.init({
-        dsn: import.meta.env.VITE_SENTRY_DSN_PUBLIC,
-        integrations: [
-            Sentry.feedbackIntegration({
-                // Additional SDK configuration goes in here, for example:
-                colorScheme: "system",
-            }),
-        ],
-    });
-}
+Sentry.init({
+    dsn: import.meta.env.VITE_SENTRY_DSN_PUBLIC,
+    integrations: [
+        Sentry.feedbackIntegration({
+            // Additional SDK configuration goes in here, for example:
+            colorScheme: "system",
+        }),
+    ],
+});
